40ccd665e025267474f013c32bafa6bbdb8da7d3
joerdav <joe.davidson.21111@gmail.com>
2024-07-12 11:23:15 +0100

    fonts

--- install/linux/fonts.sh
+++ install/linux/fonts.sh
@@ -4,6 +4,12 @@ fi
 
 ~/.nerd-fonts/install.sh
 
+if [ ! -d ~/.monaspace-fonts ]; then
+	git clone --depth=1 https://github.com/githubnext/monaspace ~/.monaspace-fonts
+fi
+
+~/.monaspace-fonts/util/install_linux.sh
+
 sudo ln -sfv $HOME/.dotfiles/system/fonts/icomoon-feather.ttf /usr/share/fonts
 sudo ln -sfv $HOME/.dotfiles/system/fonts/icomoon-feather.ttf_ /usr/share/fonts
 sudo ln -sfv $HOME/.dotfiles/system/fonts/siji.pcf /usr/share/fonts
