b2d5aca6666fdb228d8f507369c13dd61c6ab5c7
joerdav <joe.davidson.21111@gmail.com>
2024-07-12 15:46:30 +0100

    asdf

--- install/asdf_install.sh
+++ install/asdf_install.sh
@@ -3,7 +3,7 @@ if [[ $(is-macos) == 1 ]]; then
         git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
     fi
     echo "sourcing asdf"
-    . $HOME/.asdf/asdf.sh
+    source $HOME/.asdf/asdf.sh
 else
     # asdf for version management
     brew install asdf
