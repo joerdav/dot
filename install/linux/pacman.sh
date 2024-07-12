8fb82ffd17d50b8602b9250fa4d83876705d4258
joerdav <joe.davidson.21111@gmail.com>
2024-07-12 10:43:52 +0100

    pacman install

--- install/linux/pacman.sh
+++ install/linux/pacman.sh
@@ -1,35 +1,39 @@
 set -eEuo pipefail
 
-apps=(
-  atiun
-  cmake
-  coreutils
-  fzf
-  github-cli
-  git
-  # https://github.com/tj/git-extras/blob/master/Commands.md
-  git-extras
-  grep
-  gpg2
-  jq
-  neovim
-  ripgrep
-  the_silver_searcher
-  tmux
-  tree
-  wget
-  firefox
-  google-chrome
-  # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
-  spotify
-  code
-  vlc
-  wezterm
-  #font-fira-code-nerd-font
-  #font-hack-nerd-font
-  #font-roboto-mono-nerd-font
-  #font-space-mono-nerd-font
-  #font-meslo-lg-nerd-font
-)
+function install {
 
-pacman -Syu --noconfirm $(apps)
+    if [[ $(which "$1" &>/dev/null) -ne 0 ]]; then
+        echo "Installing: ${1}..."
+        sudo pacman -Syu --noconfirm "$1"
+    else
+        echo "Already installed: ${1}"
+    fi
+}
+
+
+install atiun
+install cmake
+install coreutils
+install fzf
+install github-cli
+install git
+# https://github.com/tj/git-extras/blob/master/Commands.md
+install git-extras
+install grep
+install gpg2
+install jq
+install neovim
+install ripgrep
+install the_silver_searcher
+install tmux
+install tree
+install wget
+install firefox
+install google-chrome
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+install spotify
+install code
+install vlc
+install wezterm
+
+
